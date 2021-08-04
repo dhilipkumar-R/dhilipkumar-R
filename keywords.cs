21f7e0d2a442021b837332d704c7ecaffe21f088
dhilipkumar-R <dhilip631502@gmail.com>
2021-08-04 14:22:49 +0530

    Update keywords.cs

--- keywords.cs
+++ keywords.cs
@@ -28,3 +28,37 @@ public add(out string year){
 year = 2017;
 }
 
+//readonly 
+//ReadOnly is a runtime constant.
+//In readonly fields, we can assign values in declaration and in the constructor part.
+//It can be used with static modifiers.
+
+class GFG {
+ 
+    // readonly variables
+    public readonly int myvar1;
+    public readonly int myvar2;
+ 
+    // Values of the readonly
+    // variables are assigned
+    // Using constructor
+    public GFG(int b, int c)
+    {
+ 
+        myvar1 = b;
+        myvar2 = c;
+        Console.WriteLine("Display value of myvar1 {0}, "+
+                        "and myvar2 {1}", myvar1, myvar2);
+    }
+ 
+    // Main method
+    static public void Main()
+    {
+        GFG obj1 = new GFG(100, 200);
+    }
+}
+
+//const
+//In const fields, we can only assign values in declaration part.
+//	It cannot be used with static modifiers.
+//	Const is a compile time constant.
