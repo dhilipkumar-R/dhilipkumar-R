2051cdde00c7497c52184fd9e558fdb8f70e06f5
dhilipkumar-R <dhilip631502@gmail.com>
2024-08-13 21:29:41 +0530

    Update callBindApply.js

--- callBindApply.js
+++ callBindApply.js
@@ -4,4 +4,9 @@ function greet(name){
 
 var person1 = { letter : 'dhilip' };
 
-greet.call(person1,'Hi'); //
+greet.call(person1,'Hi'); // Hi dhilip
+greet.apply(person1,['Hi']); // Hi dhilip
+
+const letterperson = greet.bind(person1);
+
+letterperson("Hi"); //Hi dhilip
