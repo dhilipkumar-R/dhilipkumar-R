fad24de37fdc3eba6f0b18ffb05b35b60a09a6ab
dhilipkumar-R <dhilip631502@gmail.com>
2024-08-13 21:31:29 +0530

    Update callBindApply.js

--- callBindApply.js
+++ callBindApply.js
@@ -10,3 +10,24 @@ greet.apply(person1,['Hi']); // Hi dhilip
 const letterperson = greet.bind(person1);
 
 letterperson("Hi"); //Hi dhilip
+
+
+/////shallow copy vs deep copy
+//shallow copy
+var a = {name: 'dhilip'};
+
+var b = a;
+
+b.name = "kumar";
+
+console.log(a); //kumar
+
+//deep copy
+
+var a = {name: 'dhilip'};
+
+var b = {...a};
+
+b.name = "kumar";
+
+console.log(a); //dhilip
